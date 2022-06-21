b49c9e68dd9abb1f4179ffa95a318e617407ef95
sosschs9 <81686317+sosschs9@users.noreply.github.com>
2022-06-21 20:53:03 +0900

    Update exe.sh

--- exe.sh
+++ exe.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-pip install re
+sudo apt-get python3-pip
+pip install --upgrade pip
+
+pip install flask
 pip install requests
 pip install bs4
-pip install time
 pip install selenium
 pip install webdriver-manager
 
-google-chrome --version
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
 
 sudo wget https://chromedriver.storage.googleapis.com/102.0.5005.61/chromedriver_linux64.zip
 
 unzip chromedriver_linux64.zip
 
-cd
 cd OSP_Team
 
 chmod +x app.py
